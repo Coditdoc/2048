234b1604e26513f96846c6891626d6e4f7de4382
Codeitdoc7 <99911985+Coditdoc@users.noreply.github.com>
2022-08-06 19:26:38 +0530

    Update Grid.js

--- Grid.js
+++ Grid.js
@@ -4,7 +4,7 @@ const CELL_GAP = 2;
 
 
 export default class Grid {
-    //making cells private is so that it can be only accesible in grid class
+ 
     #cells
     constructor(gridElement) {
         gridElement.style.setProperty("--grid-size",GRID_SIZE)
@@ -16,26 +16,57 @@ export default class Grid {
 
     }
 
+    get cells() {
+        return this.#cells
+    }
+
+    get cellsByRow() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.y] = cellGrid[cell.y] || []
+            cellGrid[cell.y][cell.x] = cell
+            return cellGrid
+        }, [])
+    }
+
+    get cellsByColumn() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell.x] || []
+            cellGrid[cell.x][cell.y] = cell
+            return cellGrid
+        }, [])
+    }
+
     get #emptyCells() {
         return this.#cells.filter(cell => cell.tile == null)
     }
-    // it will return which ever cell is empty
+
     randomEmptyCell() {
         const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
         return this.#emptyCells[randomIndex]
     }
 }
+
 class Cell {
    #cellElement
    #x
    #y
    #tile
+   #mergeTile
 
     constructor(cellElement, x,y) {
         this.#cellElement = cellElement
         this.#x = x
         this.#y = y
     }
+
+    get x() {
+        return this.#x
+    }
+
+    get y() {
+        return this.#y
+    }
+
     get tile() {
         return this.#tile
     }
@@ -46,7 +77,34 @@ class Cell {
         this.#tile.x = this.#x
         this.#tile.y = this.#y
     }
+
+    get mergeTile() {
+        return this.#mergeTile
+    } 
+
+    set mergeTile(value) {
+        this.#mergeTile = value
+        if(value == null) return
+        this.#mergeTile.x = this.#x
+        this.#mergeTile.y = this.#y
+    }
+
+    canAccept(tile) {
+        return (
+            this.tile == null || (this.mergeTile == null && this.tile.value === tile.value)
+        )
+    }
+    
+    mergeTiles() {
+        if(this.tile == null || this.mergeTile == null) return
+        this.tile.value = this.tile.value + this.mergeTile.value
+        this.mergeTile.remove()
+        this.mergeTile = null
+    }
 }
+
+
+
 
 function createCellElements(gridElement) {
     const cells = []
