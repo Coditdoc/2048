fbbfad3d042cd9cbb7981505462ea501c594387e
Codeitdoc7 <99911985+Coditdoc@users.noreply.github.com>
2022-08-06 19:53:45 +0530

    fixed

--- Tile.js
+++ Tile.js
@@ -4,17 +4,25 @@ export default class Tile {
     #y
     #value
     // randomly set 2 or 4 
-    constructor(tileContainer, value = Math.random() > .5 ? 2 : 4) {
+    constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
         this.#tileElement = document.createElement("div")
         this.#tileElement.classList.add("tile")
         tileContainer.append(this.#tileElement)
-        this.vaule = value
+        this.value = value
+    }
+
+    get value() {
+        return this.#value
     }
 
     set value(v) {
         this.#value = v
         this.#tileElement.textContent = v
+        // to determine how many times a number has be raised to power of 2
         const power = Math.log2(v)
+        const backgroundLightness = 100 - power * 9
+        this.#tileElement.style.setProperty("--background-light", `${backgroundLightness}%`)
+        this.#tileElement.style.setProperty("--text-light", `${backgroundLightness <= 50 ? 90 : 10}%`)
     }
     
     set x(value) {
@@ -26,4 +34,21 @@ export default class Tile {
         this.#y = value
         this.#tileElement.style.setProperty("--y", value)
     }
-}
+
+    remove() {
+        this.#tileElement.remove()
+      }
+    
+      waitForTransition(animation = false) {
+        return new Promise(resolve => {
+          this.#tileElement.addEventListener(
+            animation ? "animationend" : "transitionend",
+            resolve,
+            {
+              once: true,
+            }
+          )
+        })
+      }
+    }
+ 
